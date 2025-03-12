cc018455f332c80e211eec5b092af9de4b56000d
lingling1989r <lingling1989r@gmail.com>
2025-03-12 23:05:27 +0800

    update

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call npm run build
-cd dist
-type nul > .nojekyll
-git init
-git add -A
-git commit -m "deploy"
-git push -f git@github.com:lingling1989r/ai-photo-studio.git master:gh-pages
-cd ..
